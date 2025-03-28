ef361051cdcf2fa66d8d19e90658487e627934e9
zfronk <officialstevve@gmail.com>
2025-03-28 17:45:32 +0300

    default 2.0

--- timer.c
+++ timer.c
@@ -40,15 +40,98 @@ void display_time(){
 
 // Manage timer
 void set_timer(){
-	int time_in_seconds;
 	bool getting_time = true;
-
 
 	printf("Configuring timer\n");
 	printf(".................\n");
+
 	// Act as switch
+
+	// Get hours
+	int hours;
+	
 	while(getting_time){
-		printf("Input time in seconds: ");
+		printf("Input number of hours: ");
+
+
+		char input = getchar();
+		char new_line = '\n';
+
+		// Read enter
+		if(input == new_line){
+			printf("You pressed enter\n");
+			printf("\n");
+			continue;
+					
+		}
+		
+		ungetc(input, stdin); // Return input when charcter not newline for processing
+		
+		// Invalid data type
+		if(scanf("%d", &hours) != 1 || hours < 0){
+			printf("Invalid data type!\n");
+			while(getchar() != '\n'); // Remove till new line character
+			continue;
+
+		}
+		
+		else{
+			while(getchar() != '\n');
+			printf("Hours recorded!\n");
+			printf("\n");
+			getting_time = false;
+
+		}
+		
+		
+	}
+
+	getting_time = true; // Make true to rerun loop!
+
+	// Get minutes
+	int minutes; // Variable to store minutes 
+	
+	while(getting_time){
+		printf("Input number of minutes: ");
+
+		char input = getchar();
+		char new_line = '\n';
+
+		if(input == new_line){
+			printf("You pressed enter\n");
+			printf("\n");
+			continue;
+			
+		}
+
+		ungetc(input, stdin); // Return input when charcter not newline for processing
+
+		// Invalid data type
+		if(scanf("%d", &minutes) != 1 || minutes >= 60 || minutes < 0){
+			printf("Invalid data type!\n");
+			while(getchar() != '\n'); // Remove till new line character
+			printf("\n");
+			continue;
+		}
+
+		else{
+			while(getchar()!= '\n'); // Clear the buffer
+			printf("Minutes recorded!\n");
+			printf("\n");
+			getting_time = false;
+		}
+		
+	}
+
+	getting_time = true; // Make true to rerun loop!
+
+
+
+	// Get seconds
+	int seconds;
+
+	while(getting_time){
+		printf("Input number of seconds: ");
 
 		char sniff_enter = getchar();
 		char enter = '\n';
@@ -64,7 +147,7 @@ void set_timer(){
 		ungetc(sniff_enter, stdin);
 				
 		// Invalid data type
-		if(scanf("%d", &time_in_seconds) != 1 || time_in_seconds <= 0){
+		if(scanf("%d", &seconds) != 1 || seconds < 0 || seconds >= 60){
 			printf("Invalid data type!\n");
 			printf("\n");
 			while(getchar() != '\n'); // Clear till new line is read
@@ -72,33 +155,48 @@ void set_timer(){
 		}
 
 		
-
-		while(getchar() != '\n'); // Clear buffer
-		printf("Duration %d seconds!\n", time_in_seconds);
-		getting_time = false;
+		else{
+			while(getchar() != '\n'); // Clear buffer
+			printf("Seconds recorded!\n");
+			getting_time = false;
+			
+		}
 		
 	}
 
-	printf("\n");
-	printf("Timer started!\n");
-	printf("..............\n");
+		printf("\n");
+		printf(".........................\n");
+		printf("Timer set: %02d:%02d:%02d\n", hours, minutes,seconds);
 
-	int temp_time = time_in_seconds;
+		int total_seconds = (seconds + (minutes * 60) + (hours * 3600));
+		printf("%d total seconds recorded!\n", total_seconds);
+		int temp_time = total_seconds;
+		
+		printf("\n");
 
-	// Start timer
-	for(int i = 0; i < time_in_seconds; i++){
-		printf("\rTime left: %02d  ", temp_time);
-		fflush(stdout); // Immediate feedback
-		temp_time--;
-		sleep(1); // simulate a second
+		printf("Timer started!\n");
+		printf("..............\n");
 
-		// Ding dong! Time's up
-		if(temp_time == 0){
-			printf("\rTime left: %02d  ", temp_time);
-			printf("\n");
-			printf("Time's up!\n");
+		// Start timer
+		for(int i = 0; i < total_seconds; i++ ){
+			int temp_hours = temp_time / 3600; // For example i have 150 seconds divided by 3600 int stands as zero
+			int temp_minutes = (temp_time % 3600)/ 60; // Take mod(150, 3600) gives 150 but need minutes (150 / 60) gives me int one;
+			int temp_seconds = temp_time % 60; // What's left
 
-			printf("\n");
+			printf("\rTime left: %02d:%02d:%02d ", temp_hours, temp_minutes, temp_seconds);
+			fflush(stdout); // Immediate feedback
+			sleep(1); // simulate a second
+			temp_time--;
+			
+
+			
+			// Ding dong! Time's up
+			if(temp_time == 0){
+				printf("\rTime left: 00:00:00");
+				printf("\n");
+				printf("Time's up!\n");
+
+				printf("\n");
 
 			// Check if package is installed
 			if(system("which mpg123 > /dev/null 2>&1") == 0){ // return zero if present
@@ -110,6 +208,8 @@ void set_timer(){
 				// On finish
 				printf("Music terminated!\n");
 				printf("Feel free to time yourself again!\n");
+				printf("................................\n");
+				printf("\n");
 				break;
 				
 			}
@@ -124,10 +224,11 @@ void set_timer(){
 		}
 		
 	}
-
 	
 
+
 }
+
 
 
 void initiate_stopwatch(){
@@ -154,7 +255,7 @@ void initiate_stopwatch(){
 			char timer_command[50];
 			fgets(timer_command, sizeof(command), stdin);
 
-			if(strcasecmp(timer_command, "stop")){
+			if(strcasecmp(timer_command, "stop") == 0){
 				printf("Timer stopped!\n");
 				time_t end = time(NULL); // End time in uneradble format
 
@@ -193,45 +294,45 @@ void initiate_stopwatch(){
 // Control Dashboard
 void control_dashboard(){
 	printf("TERMINAL CLOCK SYSTEM++\n");
-	printf("+++++++++++++++++++++++\n");
+	printf("-----------------------\n");
 	printf("1. Display current time\n");
 	printf("2. Set alarm\n");
 	printf("3. Initiate stopwatch\n");
 	printf("4. Exit application\n");
-	printf("+++++++++++++++++++++++\n");
+	printf("-----------------------\n");
 	
 
 
 	bool collecting_choice = true;
 
 	while(collecting_choice){
-		printf("\n");
 		printf("Choose an option from the dashboard: ");
-
 		int choice;
+
 		char enter = '\n';
 		char read_input = getchar();
 		
 
 		if(read_input == enter){
 			printf("You pressed enter!\n");
-			//printf("\n");
+			printf("\n");
 			continue;
 			
 		}
 
 		ungetc(read_input, stdin); // Put back
 				
-		if(scanf("%d", &choice) != 1 || choice == '\n' ){
-				printf("Invalid input!\n");
-				while(getchar() != '\n'); // Clear buffer
-				continue;
-			}
+		if(scanf("%d", &choice) != 1){
+			printf("Invalid input!\n");
+			while(getchar() != '\n'); // Clear buffer
+			printf("\n");
+			continue;
+
+		}
 
 
 		if(choice == 1){
 			getchar(); // Remove newline
-			//printf("\n");
 			display_time();
 			
 		}
@@ -250,9 +351,16 @@ void control_dashboard(){
 
 		else if(choice == 4){
 			getchar();
-			printf("Have a nice time🖥️  buddy.  \n");
+			printf("Have a nice time buddy!\n");
 			printf("\n");
 			break;
+		}
+
+		else{
+			while(getchar() != '\n'); // Remove new line
+			printf("Choose valid options!\n");
+			printf("\n");
+			continue;
 		}
 
 
